739b9807b1ca02dcee24defe0d598cbae7a092d9
Jesse SMith <jesse.lee.smith@gmail.com>
2017-12-18 17:19:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {theBeatles: 'Hey Jude'};
 
 function updatePlaylist(playlist, artistName,songTitle){
-  Object.assign({}, playlist, {[artistName]: songTitle});
+  return Object.assign({}, playlist, {[artistName]: songTitle});
 }
